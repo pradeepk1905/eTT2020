bc21a5d73332b735ae1da15addb1cb69f8261271
Pradeep K <prdp.dev@gmail.com>
2020-06-30 22:54:11 +0530

    home done except grouping

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ var app = new Vue({
         fri: '',
         sat: '',
         sun: '',
-        showTasks: {},
         mobileView: false,
         showView: 'mon',
         dayEntries: {},
@@ -24,20 +23,17 @@ var app = new Vue({
             "fri",
             "sat"
         ],
-        data: {},
         total: {},
-        entries: [
-            { date: "20200630", hours: 8, desc: "", task: 1 },
-            { date: "20200629", hours: 3.5, desc: "", task: 124 },
-            { date: "20200629", hours: 4.5, desc: "", task: 235 }
-        ],
+        currentTask: {
+            desc: "",
+            hours: 0
+        },
+        entries: [],
         selectedTasks: [
             1, 124, 235
         ],
         project: [
             "Leave",
-            "GMOne",
-            "DCF"
         ],
         tasks: {
             1: { project: "Leave", title: "Annual Leave" },
@@ -77,23 +73,36 @@ var app = new Vue({
                     date_f: d.format("MM/DD")
                 };
             }
-
-            console.log(this.dayEntries);
             this.calcEntries();
         },
         calcEntries: function () {
             this.days.forEach(d => {
-                console.log(this.dayEntries[d]);
+                this.dayEntries[d].tasks = {};
                 this.selectedTasks.forEach(t => {
                     this.entries.forEach(e => {
-                        this.dayEntries[d].tasks = {};
-                        if (e.date == this.dayEntries[d].date) {
+                        if (e.date == this.dayEntries[d].date && e.task == t) {
                             this.dayEntries[d].tasks[t] = e;
                         }
                     })
                 });
-                //this.total[d] = 0;
+                this.calculateTotal(d);
             });
+        },
+        openDetailsModal: function (day, tid) {
+            const task = this.getTask(day, tid);
+            if (task) {
+                this.currentTask = task;
+                new bootstrap.Modal(document.getElementById('exampleModal'), {}).show();
+            }
+        },
+        getTask: function (day, tid) {
+            return (this.dayEntries[day].tasks && this.dayEntries[day].tasks[tid]) ? this.dayEntries[day].tasks[tid] : undefined;
+        },
+        getHours: function (day, tid) {
+            return (this.dayEntries[day].tasks && this.dayEntries[day].tasks[tid]) ? this.dayEntries[day].tasks[tid].hours : 0;
+        },
+        getStatus: function (day, tid) {
+            return (this.dayEntries[day].tasks && this.dayEntries[day].tasks[tid]) ? this.dayEntries[day].tasks[tid].status : 'pending';
         },
         winWidth: function () {
             var w = window.innerWidth;
@@ -114,67 +123,105 @@ var app = new Vue({
         },
         changeView: function (day) {
             this.showView = day;
-            this.data = JSON.parse(JSON.stringify(this.data));
         },
         addEvent({ type, target }) {
             this.loading = false;
-            this.data[target.dataset.day][target.dataset.key].val = target.value;
-            this.calculateTotal(target.dataset.day);
+            var val = -1;
+            if (parseFloat(target.value) >= 0) {
+                val = target.value
+            } else if (target.value == "") {
+                val = 0;
+            }
+            if (val >= 0) {
+                if (val > 0 && !this.dayEntries[target.dataset.day].tasks[target.dataset.key]) {
+                    //create new entry and process
+                    const entry = {
+                        hours: val,
+                        desc: '',
+                        date: this.dayEntries[target.dataset.day].date,
+                        task: target.dataset.key,
+                        fromTime: moment().format("HH:mm"),
+                        toTime: moment().add(val, 'h').format("HH:mm")
+                    };
+                    this.dayEntries[target.dataset.day].tasks[target.dataset.key] = entry;
+                    this.entries.push(entry);
+                } else if (val >= 0 && this.dayEntries[target.dataset.day].tasks[target.dataset.key]) {
+                    //update existing entry
+                    this.dayEntries[target.dataset.day].tasks[target.dataset.key].hours = val;
+                    this.entries.forEach((e, index, object) => {
+                        if (e.task == target.dataset.key && e.date == this.dayEntries[target.dataset.day].date) {
+                            if (val == 0) {
+                                object.splice(index, 1);
+                                this.dayEntries[target.dataset.day].tasks[target.dataset.key] = undefined;
+                            } else {
+                                e.hours = val;
+                                e.fromTime = moment().format("HH:mm");
+                                e.toTime = moment(e.fromTime, "HH:mm").add(val, 'h').format("HH:mm")
+                            }
+                        }
+                    });
+                }
+                this.calculateTotal(target.dataset.day);
+            }
+        },
+        addModalEvent: function () {
+            this.entries.forEach(e => {
+                if (e.date == this.currentTask.date && e.task == this.currentTask.task) {
+                    e = this.currentTask;
+                    e.toTime = moment(e.fromTime, "HH:mm").add(e.hours, 'h').format("HH:mm")
+                }
+            });
+            this.dateloader();
         },
         openTasksModal: function () {
             new bootstrap.Modal(document.getElementById('tasksModal'), {}).show();
         },
         calculateTotal: function (day) {
-            var data = JSON.parse(JSON.stringify(this.data));
+            var data = JSON.parse(JSON.stringify(this.dayEntries));
             var total = 0.0;
-            Object.keys(data[day]).forEach(function (key) {
-                if (parseFloat(data[day][+key].val) > 0)
-                    total = +(total + parseFloat(data[day][+key].val));
+            Object.keys(data[day].tasks).forEach(function (key) {
+                if (parseFloat(data[day].tasks[key].hours) > 0)
+                    total = +(total + parseFloat(data[day].tasks[key].hours));
             });
             var md = moment.duration(total, 'hours');
             var s = (md._data.hours > 0) ? md._data.hours + "h " : "";
             s = s.concat((md._data.minutes > 0) ? md._data.minutes + "m " : "");
             this.total[day] = s;
             this.$forceUpdate();
-        },
-        toggleTasks: function (p) {
-            this.showTasks[p] = !this.showTasks[p];
-            this.$forceUpdate();
+            localStorage.setItem("dayEntries", JSON.stringify(data));
+            localStorage.setItem("entries", JSON.stringify(this.entries));
+            this.loading = false;
         },
         showTask: function (p, tid, t) {
             return t.project == p && !this.selectedTasks.includes(+tid);
         },
         addTask: function (tid, t) {
-            this.days.forEach(d => {
-                this.data[d][tid] = { val: 0, status: "pending" }
-            });
             this.selectedTasks.push(tid);
             this.$forceUpdate();
-            console.log(bootstrap.Modal.getInstance(document.getElementById('tasksModal')).hide())
-        },
-        getTask: function (tId) {
-            this.selectedTasks.push(tId);
-        },
+            bootstrap.Modal.getInstance(document.getElementById('tasksModal')).hide()
+        }
     },
     mounted() {
-        this.days.forEach(d => {
-            this.data[d] = {
-                0: {}
-            };
-            this.selectedTasks.forEach(t => {
-                this.data[d][t] = { val: 0, status: "pending" }
-            });
-            this.total[d] = 0;
-        });
-        this.project.forEach(p => {
-            this.showTasks[p] = false;
-        });
+        if (localStorage.getItem("entries") == undefined) {
+            this.entries = [
+                {
+                    date: moment(new Date()).format("YYYYMMDD"), hours: 8, desc: "", task: 1,
+                    fromTime: moment("09:00", "HH:mm").format("HH:mm"), toTime: moment("09:00", "HH:mm").add(8, 'h').format("HH:mm")
+                },
+                {
+                    date: moment(new Date()).subtract(1, 'd').format("YYYYMMDD"), hours: 3.5, desc: "", task: 124, status: 'rejected',
+                    fromTime: moment("09:00", "HH:mm").format("HH:mm"), toTime: moment("09:00", "HH:mm").add(3.5, 'h').format("HH:mm")
+                },
+                {
+                    date: moment(new Date()).add(1, 'd').format("YYYYMMDD"), hours: 4.5, desc: "", task: 235, status: 'approved',
+                    fromTime: moment("09:00", "HH:mm").format("HH:mm"), toTime: moment("09:00", "HH:mm").add(4.5, 'h').format("HH:mm")
+                }
+            ];
+        } else {
+            this.entries = JSON.parse(localStorage.getItem("entries"));
+        }
         this.weeklyDatePicker = moment(new Date()).format("YYYY-MM-DD")
         this.dateloader()
         this.winWidth()
-        this.days.forEach(d => {
-            this.calculateTotal(d)
-        });
-        this.loading = false;
     }
 });
